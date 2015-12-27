e9a705821d2ab1a9abe993036c05c123fcf4250f
RMSailors <theinformaticist@gmail.com>
2015-12-27 14:44:10 -0500

    initial project version

---
+++ run_analysis.R
@@ -0,0 +1,20 @@
+## Getting and Cleaning Data Course Project
+
+## load training set
+
+## load test set
+
+## 1.  merge sets
+
+## 2.  extract mean and standard deviation from merged set
+
+## 3.  name activities using descriptive names
+
+## 4.  label dataset with descriptive variable names
+
+## 5. create independent tidy dataset with the average for each activity and each subject based on dataset in item 4
+
+## output dataset
+   
+   
+   write.table(TidyDataset, file = "rmsailors_output.tex", row.names = FALSE)
